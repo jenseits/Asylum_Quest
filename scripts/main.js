122af03a9125ff137c4d60717a3180997f6182dc
jenseits <marie-a.m@hotmail.fr>
2018-01-07 23:28:09 +0100

    rework of index - WIP

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
-if(!localStorage.getItem('name')) {
+if(!localStorage.getItem('fname')) {
     setUserName();
 } else {
-    var storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Hello' + storedName + ','+ ' I hope you are ready to descend into the darkest parts of your soul.' ;
+    var storedName = localStorage.getItem('fname');
+    myHeading.textContent = 'Hello' + storedName + ', I hope you are ready to descend into the darkest parts of your soul. Asylum Escape is a place from which there is no return home, or... is there ?' ;
 }
